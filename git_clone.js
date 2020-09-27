3d9f43d891a1c11209c56f3720ba4443c91dfeaa
JoaMoreno <tec.ignaciomoreno@gmail.com>
2020-09-27 16:07:24 -0300

    add registry script and dotenv

--- git_clone.js
+++ git_clone.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+require("dotenv").config();
 
 const gitRepo = process.argv.slice(2).join()
 
 const regex = /gitlab/gi;
-const url = gitRepo.replace(regex, 'ijmoreno@gitlab');
+const url = gitRepo.replace(regex, `${process.env.USERNAME_GL}@gitlab`);
 let project = gitRepo.split('/');
 project = project[project.length - 1].replace('.git','');
 
